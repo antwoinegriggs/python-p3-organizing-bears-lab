ef2a88ce228ba94713dce97b590125465d3a8e6f
antwoine <antwoinegriggs@outlook.com>
2023-07-26 21:15:17 -0400

    complete lab

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,21 +1,21 @@
 select_all_female_bears_return_name_and_age = """
     SELECT
-        bear.name,
-        bear.age
+        name,
+        age
     FROM bears
     WHERE sex = 'F'
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
     SELECT
-        bear.name
+        name
     FROM bears
     ORDER BY ASC
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
     SELECT
-        bear.name
+        name
     FROM bears
     WHERE alive = true
     ORDER BY ASC
@@ -23,17 +23,17 @@ select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = ""
 
 select_oldest_bear_and_returns_name_and_age = """
     SELECT
-        bears.name,
-        bears.age
+        name,
+        age
     FROM bears
-    ORDER BY bears.age DESC
+    ORDER BY age DESC
     LIMIT 1
 """
 select_youngest_bear_and_returns_name_and_age = """
     SELECT
-        bears.name,
-        bears.age
+        name,
+        age
     FROM bears
-    ORDER BY bears.age ASC
+    ORDER BY age ASC
     LIMIT 1
 """
