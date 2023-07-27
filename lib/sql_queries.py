e49abb6413ff31d1ef603a5a22b023e2a23b3a29
antwoine <antwoinegriggs@outlook.com>
2023-07-26 21:21:20 -0400

    complete lab

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -10,14 +10,14 @@ select_all_bears_names_and_orders_in_alphabetical_order = """
     SELECT
         name
     FROM bears
-    ORDER BY DESC
+    ORDER BY name DESC
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
     SELECT
         name, age
     FROM bears
-    WHERE alive = true
+    WHERE alive = 1
     ORDER BY age ASC
 """
 
