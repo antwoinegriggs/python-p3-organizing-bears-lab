6297d3814b40827fdd7eb7d4237f64831498a929
antwoine <antwoinegriggs@outlook.com>
2023-07-26 20:45:26 -0400

    complete lab

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTERGER PRIMARY KEY,
+  name TEXT,
+  age INTERGER,
+  sex TEXT,
+  color TEXT,
+  temperment TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, sex, color, temperment, alive2) 
+VALUES 
+('Mr. Chocolate', 35, 'M', 'Brown','Active', 1),
+('Rowdy', 19, 'M', 'Gold','Aggressive', 0),
+('Tabitha', 23, 'F', 'Orange','Quiet', 1),
+('Sergeant Brown', 42, 'M', 'Gray','Leadership', 1),
+('Melissa', 25, 'F', 'Pink','Outgoing', 1),
+('Grinch', 28, 'M', 'Green','Impulsive', 0),
+('Wendy', 30, 'F', 'Purple','Peaceful', 1),
+(NULL, 1, 'F', 'Black','Restless', 1)
